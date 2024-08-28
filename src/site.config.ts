d40f1eb784ba987f5003ca216a81a68fb552a313
felixng <ngthinh302@gmail.com>
2024-08-28 11:59:52 -0400

    webmentions

--- src/site.config.ts
+++ src/site.config.ts
@@ -25,7 +25,6 @@ export const siteConfig: SiteConfig = {
 	title: "Felix Yammers On...",
 	webmentions: {
 		// Webmention.io API endpoint. Get your own here: https://webmention.io/, and follow this blog post: https://astro-cactus.chriswilliams.dev/posts/webmentions/
-		// TODO: Add this
 		link: "https://webmention.io/api/mentions.html?token=2PbfMEVw3uH3nr3amwJVZQ",
 	},
 };
